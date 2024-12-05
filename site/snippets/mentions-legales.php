3ae2833ef55691f1eef35c86e3cee4ecf124eb0d
Matleftod <97225801+Matleftod@users.noreply.github.com>
2024-12-05 16:28:09 +0100

    fix mentions

--- site/snippets/mentions-legales.php
+++ site/snippets/mentions-legales.php
@@ -2,10 +2,8 @@
   <div class="mentions__container">
     <h1 class="mentions__title"><?= $page->title()->html() ?></h1>
     <div class="mentions__content">
-      <?php if ($page->content()): ?>
-        <?= $page->content()->kt(); ?>
-        <?php var_dump($page->title()->value()); ?>
-    <?php var_dump($page->content()->value()); ?>
+      <?php if ($page->text()): ?>
+        <?= $page->text()->kt(); ?>
       <?php else: ?>
         <p>Le contenu n'a pas été trouvé.</p>
       <?php endif; ?>
