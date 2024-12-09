9f97863343c60a941b7c20e0faa71fed32b41e51
Matleftod <97225801+Matleftod@users.noreply.github.com>
2024-12-09 16:00:39 +0100

    update message

--- site/plugins/notifications/index.php
+++ site/plugins/notifications/index.php
@@ -3,25 +3,13 @@
 Kirby::plugin('custom/notifications', [
     'areas' => [
         'messages' => function ($kirby) {
-            $unreadMessages = page('messages')->childrenAndDrafts()->filterBy('customstatus', 'Non lu')->count();
             return [
                 'label' => 'Messages',
                 'icon' => 'email',
                 'link' => 'pages/messages',
                 'menu' => true,
-                'data' => [
-                    'unread' => $unreadMessages,
-                ],
             ];
         },
-    ],
-    'hooks' => [
-        'kirby.page.changeStatus:after' => function ($newPage) {
-            // Lorsque le statut d'un message est modifié, rafraîchissez les notifications
-            if ($newPage->intendedTemplate()->name() === 'message') {
-                kirby()->cache('custom.notifications')->flush();
-            }
-        }
-    ],
+    ]
 ])
 ?>
