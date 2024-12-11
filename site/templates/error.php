5ed8c8fa7ab78ebe6a9613edc68c1f896d25966d
Matleftod <97225801+Matleftod@users.noreply.github.com>
2024-12-11 16:14:39 +0100

    error page

---
+++ site/templates/error.php
@@ -0,0 +1,22 @@
+<?php snippet('header') ?>
+
+<main class="error">
+  <div class="error__container">
+    <h1 class="error__title">Page non trouvée</h1>
+    <p class="error__message">Désolé, la page que vous recherchez n'existe pas ou a été déplacée.</p>
+    
+    <ul class="error__suggestions">
+      <li>
+        <a href="/">Retour à la page d'accueil</a>
+      </li>
+      <li>
+        <a href="/contact">Contactez-nous</a>
+      </li>
+      <li>
+        <a href="/blog">Consultez notre blog</a>
+      </li>
+    </ul>
+  </div>
+</main>
+
+<?php snippet('footer') ?>
